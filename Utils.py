621d6e040de3eebbbd75a2649653df3bb16aa1d4
arshultroud <90410083+arshultroud@users.noreply.github.com>
2021-09-10 15:55:58 +0530

    Add files via upload

---
+++ Utils.py
@@ -0,0 +1,28 @@
+RAID = [
+    "https://youtube.com/shorts/JvdpjWCioSQ?feature=share",
+    "https://youtube.com/shorts/7h8gYj1yYcU?feature=share",
+    "SUB4SUB DM ME 1V1 3V3",
+    "MSG ME",
+    "https://youtu.be/3SjieZYWLoI",
+    "https://youtube.com/shorts/FSMogQvFSE4?feature=share",
+    "https://youtube.com/shorts/UrZUskSUi3I?feature=share",
+    "https://youtu.be/nKiP4XhRcPs",
+    "https://youtube.com/shorts/ipMnMyaSEX0?feature=share",
+    "DUDH HILAAUNGA TERI VAHEEN KE UPR NICHE 🆙🆒😙",
+    "1v1v1v12v2v2v2 3v3v3v34 v4v4v45v5v5v5 6v6v6v6 10v10v10 Parmanent SUBSCRIBE Subscribe And Send Screenshort plz",
+]
+
+
+RRAID = [
+    "https://youtube.com/shorts/JvdpjWCioSQ?feature=share",
+    "https://youtube.com/shorts/7h8gYj1yYcU?feature=share",
+    "SUB4SUB DM ME 1V1 3V3",
+    "MSG ME",
+    "https://youtu.be/3SjieZYWLoI",
+    "https://youtube.com/shorts/FSMogQvFSE4?feature=share",
+    "https://youtube.com/shorts/UrZUskSUi3I?feature=share",
+    "https://youtu.be/nKiP4XhRcPs",
+    "https://youtube.com/shorts/ipMnMyaSEX0?feature=share",
+    "DUDH HILAAUNGA TERI VAHEEN KE UPR NICHE 🆙🆒😙",
+    "1v1v1v12v2v2v2 3v3v3v34 v4v4v45v5v5v5 6v6v6v6 10v10v10 Parmanent SUBSCRIBE Subscribe And Send Screenshort plz",
+]
